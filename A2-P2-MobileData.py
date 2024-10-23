a503973a302df0f3841fb3873d7bdfb28e1e8ece
WilliamEWilson <william_wilson_1990@hotmail.com>
2024-10-23 18:50:51 -0300

    Assignment 2, part 2 ready for marking

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,19 +5,32 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #:w0457754
+#Student Name: William Wilson
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    #0-200Mb $20 flat rate
+    #200-500Mb $0.105 per Mb
+    #500Mb - 1Gb (1000Mb) $0.110 per Mb
+    #1Gb+ $118.00 flat rate
 
+    #welcome message
+    print("Welcome to Erewhon Mobile. I am Agent William. In order to help you better, can I get you to enter your data usage.")
 
+    #gather data usage form client
+    dataUsage = int(input("Enter data usage (MB): "))
 
+    #break down data usage and cost 
+    if dataUsage <= 200:
+        ratePrice = 20
+    elif dataUsage == 201 or dataUsage <= 500:
+        ratePrice = dataUsage * 0.105
+    elif dataUsage == 500 or dataUsage <=1000:
+        ratePrice = dataUsage * 0.110
+    else:
+        ratePrice = 118
 
-
-
-
-
-    # YOUR CODE ENDS HERE
+    #display to client the price for data usage
+    print("Your total charge for data usage is: ${0:.2f}".format(ratePrice))
 
 main()
